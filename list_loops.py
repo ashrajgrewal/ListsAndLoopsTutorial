476e8eea77f8c2aede83439b0dfaa65c7af90ac1
Ashraj Grewal <ashraj.grewal10@gmail.com>
2020-10-06 10:57:31 -0700

    Update list_loops.py

--- list_loops.py
+++ list_loops.py
@@ -1,1 +1,17 @@
 #AshrajGrewal Lists and Loops Tutorial
+#Q1
+songs = ["ROCKSTAR", "Do It", "For The Night"]
+#Q2
+print(songs[0])
+print(songs[2])
+print(songs[1:3])
+#Q3
+songs[2] = "We are Young"
+print(songs)
+#Q4
+songs.append("Headlines")
+songs.extend(["Twin Turbo", "ElDorado"])
+print(songs)
+#songs.remove("ElDorado")
+#del songs[5]
+print(songs)
